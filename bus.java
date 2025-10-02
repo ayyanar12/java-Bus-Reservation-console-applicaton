3880f269f6af1c11c63b3524bd27e1a83f4300c5
ayyanar12 <146554499+ayyanar12@users.noreply.github.com>
2025-10-02 08:22:56 +0530

    Add files via upload

---
+++ bus.java
@@ -0,0 +1,15 @@
+package login;
+
+public class bus {
+	int busno;
+	int buscapacity;
+	boolean busac;
+	int bookingseat;
+	bus(int busno,int buscapacity,boolean busac){
+		this.busno=busno;
+		this.buscapacity=buscapacity;
+		this.busac=busac;
+		bookingseat=0;
+	}
+
+}
