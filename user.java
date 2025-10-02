2064dfaf2235ad711b9893735a2d92d6b744a308
ayyanar12 <146554499+ayyanar12@users.noreply.github.com>
2025-10-02 08:22:04 +0530

    Add files via upload

---
+++ user.java
@@ -0,0 +1,14 @@
+package login;
+
+public class user {
+	String name;
+	String password;
+	String email;
+	
+	user(String name,String password,String mail){
+		this.name=name;
+		this.password=password;
+		this.email=mail;
+	}
+
+}
